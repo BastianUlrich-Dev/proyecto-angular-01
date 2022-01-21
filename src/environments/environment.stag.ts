920505a6c51cf9d161d5ab6b4944edd6713620e8
Marmota-hub <bastianjorquera8@gmail.com>
2022-01-21 19:36:22 -0300

    nuevas importaciones

--- src/environments/environment.stag.ts
+++ src/environments/environment.stag.ts
@@ -1,5 +1,13 @@
 export const environment = {
   production: true,
   url_api: 'https://platzi-store-stag.herokuapp.com',
-
+  firebase: {
+    apiKey: "AIzaSyAhP_1uSZvpYOYMhjQsAxaFOOBljy1gaSU",
+    authDomain: "proyecto-angular-a964c.firebaseapp.com",
+    projectId: "proyecto-angular-a964c",
+    storageBucket: "proyecto-angular-a964c.appspot.com",
+    messagingSenderId: "1020633872650",
+    appId: "1:1020633872650:web:26201f86e04394363722f3"
+  
+  }
 };
