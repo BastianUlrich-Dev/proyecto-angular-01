5c67a56d25c29420925e089bd58b00084c8aaba5
Marmota-hub <bastianjorquera8@gmail.com>
2022-01-21 19:13:36 -0300

    Se agrego firebase

--- src/environments/environment.stag.ts
+++ src/environments/environment.stag.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  url_api: 'https://platzi-store-stag.herokuapp.com'
+  url_api: 'https://platzi-store-stag.herokuapp.com',
+  const firebaseConfig = {
+    apiKey: "AIzaSyAhP_1uSZvpYOYMhjQsAxaFOOBljy1gaSU",
+    authDomain: "proyecto-angular-a964c.firebaseapp.com",
+    projectId: "proyecto-angular-a964c",
+    storageBucket: "proyecto-angular-a964c.appspot.com",
+    messagingSenderId: "1020633872650",
+    appId: "1:1020633872650:web:26201f86e04394363722f3"
+  };
 };
