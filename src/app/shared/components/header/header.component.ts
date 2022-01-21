421e9c2905d653accd2b0b8473356caa70779061
Marmota-hub <bastianjorquera8@gmail.com>
2022-01-21 13:19:22 -0300

    carro de compras corregido

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { CartService } from 'src/app/core/services/cart.service';
+import { ProductsComponent } from 'src/app/product/components/products/products.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit { 
 
-  constructor() { }
+total$: Observable<number>;
+
+  constructor(
+    private cartService: CartService
+    ) {
+    this.total$ = this.cartService.cart$
+    .pipe(
+      map(products => products.length)
+    )
+   }
 
   ngOnInit() {
   }
+
+
 
 }
