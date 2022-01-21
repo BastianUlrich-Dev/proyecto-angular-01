37bf33361416ff7ba2668789d8497e9d03a670f9
Marmota-hub <bastianjorquera8@gmail.com>
2022-01-21 19:19:37 -0300

    errores corregidos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,5 @@
 export const environment = {
   production: true,
   url_api: 'https://platzi-store.herokuapp.com',
-  const firebaseConfig = {
-    apiKey: "AIzaSyAhP_1uSZvpYOYMhjQsAxaFOOBljy1gaSU",
-    authDomain: "proyecto-angular-a964c.firebaseapp.com",
-    projectId: "proyecto-angular-a964c",
-    storageBucket: "proyecto-angular-a964c.appspot.com",
-    messagingSenderId: "1020633872650",
-    appId: "1:1020633872650:web:26201f86e04394363722f3"
-  }
+
 };
